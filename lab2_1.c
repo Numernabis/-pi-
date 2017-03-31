9eb159834c6f7b237c7ac83df5d5135c7273111c
Numernabis <luigi.zielone@gmail.com>
2017-04-01 00:28:05 +0200

    tiny fixes

--- lab2_1.c
+++ lab2_1.c
@@ -13,32 +13,32 @@ int main()
     scanf("%lu", &b);
     int i, j = 0, s = 1;
 
-   	int t1[N], t2[N], res[2*N];
+	int t1[N], t2[N], res[2*N];
 
-   	for(i = 0; i < N; i++) {
-   		res[i] = 0;
-   		t1[i] = a % 10;
-   		a /= 10;
-   		t2[i] = b % 10 * s;
-   		b /= 10;
-   		s *= 10;
-   	}
+	for(i = 0; i < N; i++) {
+		res[i] = 0;
+		t1[i] = a % 10;
+		a /= 10;
+		t2[i] = b % 10 * s;
+		b /= 10;
+		s *= 10;
+	}
 
-   	while(j < 100) {
-   		for(i = 0; i < N; i++) {
-	   		res[i] += t1[i] * t2[j];
-	   	}
-	   	for(i = 0; i < N - 1; i++) {
-	   		res[i + 1] += res[i] / 10;
-	   		res[i] %= 10;
-	   	}
-	   	j++;
+	while(j < 100) {
+		for(i = 0; i < N; i++) {
+			res[i] += t1[i] * t2[j];
+		}
+		for(i = 0; i < N - 1; i++) {
+			res[i + 1] += res[i] / 10;
+			res[i] %= 10;
+		}
+		j++;
 	}
 
 	printf("Product: ");
-   	for(i = N - 1; i >= 0 && res[i] == 0; i--);
-   	for(; i >= 0; i--)
-   		printf("%d", res[i]);
+	for(i = N - 1; i >= 0 && res[i] == 0; i--);
+	for(; i >= 0; i--)
+	printf("%d", res[i]);
 
     printf("\n");
     return 0;
